59efea53fa7f1165adf79e2dc0f49bcb7f08e1fb
mateusmaiadb <mateus.maia@dbserver.com.br>
2021-11-11 18:54:55 -0300

    PhotoComponent criado para rganizar template, bootstrap configurado

---
+++ airbnb-angular/src/app/photo/photo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'db-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+
+    description = 'Logo Airbnb'
+    url = 'https://news.airbnb.com/wp-content/uploads/sites/4/2017/01/airbnb_vertical_lockup_web.png?fit=451%2C493'
+}
